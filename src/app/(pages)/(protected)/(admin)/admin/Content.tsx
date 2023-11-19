eebb18311feacbe124127a46d2d2d4c8290f229a
Rafixx <rafal987.1@o2.pl>
2023-11-19 15:43:37 +0100

    feat: create basic admin dashboard

---
+++ src/app/(pages)/(protected)/(admin)/admin/Content.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useAppSelector, useAppDispatch } from '@/store/hooks';
+import { deleteAllReserations } from '@/store/features/reservations/reservationsSlice';
+
+export const Content = () => {
+  const dispatch = useAppDispatch();
+
+  const reservations = useAppSelector((state) => state.reservations);
+  const employees = useAppSelector((state) => state.employees);
+  const services = useAppSelector((state) => state.services);
+
+  return (
+    <>
+      <ul className='flex flex-col gap-4'>
+        {reservations.map((reservation) => (
+          <li key={reservation.id} className='flex items-center justify-evenly'>
+            <span className='w-96 border'>
+              {
+                services.find((service) => service.id === reservation.serviceId)
+                  ?.title
+              }
+            </span>
+            <span>
+              {
+                employees.find(
+                  (employee) => employee.id === reservation.executorId
+                )?.name
+              }
+            </span>
+            <span>{reservation.reservationDate?.day}</span>
+            <span>{reservation.reservationDate?.hour}</span>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => dispatch(deleteAllReserations())}>
+        Usuń rezerwacje
+      </button>
+    </>
+  );
+};
